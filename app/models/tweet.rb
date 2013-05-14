f91fd31481f71029a8ab0f01ee2f8c684150e7eb
kate <rinarish@me.com>
2013-05-14 23:17:46 +0400

    part of show_follow

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,8 +3,13 @@ class Tweet < ActiveRecord::Base
   default_scope order: 'created_at DESC'
   attr_accessible :content
 
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
+
   def self.from_users_following_by(user)
-    following_user_ids = user.following_user_ids
-    where("user_id IN (?) OR user_id = ?", following_user_ids, user)
+    followeing_ids = "SELECT following_id FROM relationships
+                         WHERE followers_id = :user_id"
+    where("user_id IN (#{following_ids}) OR user_id = :user_id",
+          user_id: user.id)
   end
 end
